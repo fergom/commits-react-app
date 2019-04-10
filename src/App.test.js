ccf0504af8cf13d011154c5c550019126cc39cb8
Fernando Gómez <al286313@uji.es>
2019-04-10 18:22:25 +0200

    App test added.

--- src/App.test.js
+++ src/App.test.js
@@ -1,9 +1,33 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import { shallow } from 'enzyme';
 import App from './App';
+import HeaderComponent from "./components/header/header";
+import SpinnerComponent from "./components/spinner/spinner";
+import { BrowserRouter as Router } from 'react-router-dom';
 
-it('renders without crashing', () => {
-  const div = document.createElement('div');
-  ReactDOM.render(<App />, div);
-  ReactDOM.unmountComponentAtNode(div);
+const setUp = () => {
+    return shallow(<App />);
+};
+
+describe('AppComponent', () => {
+
+    let component;
+
+    beforeEach(() => {
+        component = setUp();
+    });
+
+    it('renders without crashing', () => {
+        const div = document.createElement('div');
+        ReactDOM.render(<App />, div);
+        ReactDOM.unmountComponentAtNode(div);
+    });
+
+    it('contains the spinner, the header and the router', () => {
+      expect(component.find(SpinnerComponent).length).toEqual(1);
+      expect(component.find(HeaderComponent).length).toEqual(1);
+      expect(component.find(Router).length).toEqual(1);
+    });
 });
+
